2b155302dbed86bd42dff96d99dd1b4ce3136dda
sofiahag <sofia.h4glund@gmail.com>
2023-09-23 20:40:05 +0200

    add confetti

--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import ConfettiExplosion from 'react-confetti-explosion';
 
 import { pics } from "../assets/cards/cards"
 import lightBackCard from "../assets/uni-back-light.png"
@@ -23,6 +24,13 @@ function Game() {
         } else {
           setTheme('light');
         }
+    };
+
+    const confetti = {
+        force: 0.5,
+        duration: 4500,
+        particleCount: 2000,
+        width: 4000,
     };
 
     function flipPic(pic, index) {
@@ -84,6 +92,7 @@ function Game() {
 
     return (
         <div className="md:h-screen md:flex md:items-center md:justify-center">
+            <div className="h-full justify-center fixed">{ creds === 2 && <ConfettiExplosion config={confetti} /> } </div>
             <div className="columns-1 lg:ml-12 lg:mr-5 md:ml-6 md:mr-1 max-sm:columns-2 max-sm:justify-center max-sm:my-5 max-sm:flex 
                 max-sm:items-center max-sm:ml-0 max-sm:mr-0">
                 <h3 className="mb-4 lg:ml-5 md:ml-5 font-sofia text-lg dark:text-white max-sm:ml-0 max-sm:mr-2 max-sm:mt-4 max-sm:text-base">
