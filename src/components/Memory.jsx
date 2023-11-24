26db6909cd00a8ac1ad38420182dd08570fbadf4
sofiahag <sofia.h4glund@gmail.com>
2023-11-24 19:49:42 +0100

    cleanup code

--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ConfettiExplosion from "react-confetti-explosion";
-
 import { pics } from "../assets/cards/cards";
 import darkBackCard from "../assets/uni-back-dark.png";
 
-
 function Game() {
-
-    const [picsArray, setpicsArray] = useState([]);
+    const [picsArray, setPicsArray] = useState([]);
     const [cardsPicked, setCardsPicked] = useState([]);
     const [cardsPickedIds, setCardsPickedIds] = useState([]);
     const [creds, setCreds] = useState(0);
@@ -29,46 +26,40 @@ function Game() {
         width: 4000,
     };
 
-    function flipPic(pic, index) {
-
-        if (cardsPickedIds?.length === 1 && cardsPickedIds[0] === index) {
-            return
+    const flipPic = (pic, index) => {
+        if (cardsPickedIds.length === 1 && cardsPickedIds[0] === index) {
+            return;
         }
 
-        if (cardsPicked?.length < 2) {
-
+        if (cardsPicked.length < 2) {
             setIsRunning(true);
-            setCardsPicked(cardsPicked => cardsPicked?.concat(pic))
-            setCardsPickedIds(cardsPickedIds => cardsPickedIds?.concat(index))
+            setCardsPicked([...cardsPicked, pic]);
+            setCardsPickedIds([...cardsPickedIds, index]);
 
-            if (cardsPicked?.length === 1) {
-                
+            if (cardsPicked.length === 1) {
                 if (cardsPicked[0] === pic) {
-                    setCreds(creds => creds + 2)
-                    setFlippedCards(flippedCards => flippedCards?.concat([cardsPicked[0], pic]))
+                    setCreds((creds) => creds + 2);
+                    setFlippedCards([...flippedCards, cardsPicked[0], pic]);
                 }
                 setTimeout(() => {
-                    setCardsPickedIds([])
-                    setCardsPicked([])
-                }, 2000)
-                
-            } 
+                    setCardsPickedIds([]);
+                    setCardsPicked([]);
+                }, 2000);
+            }
         }
-    }
+    };
 
-    function isCardPicked(pic, index) {
-        return cardsPickedIds?.includes(index) || flippedCards?.includes(pic)
-    }
+    const isCardPicked = (pic, index) => cardsPickedIds.includes(index) || flippedCards.includes(pic);
 
-    function shuffleArray(array) {
+    const shuffleArray = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
-        return array
-    }
+        return array;
+    };
 
-    function startOver() {
+    const startOver = () => {
         setCardsPickedIds([]);
         setCardsPicked([]);
         setCreds(0);
@@ -76,54 +67,44 @@ function Game() {
         setIsRunning(false);
         setTime(0);
         setDarkness('brightness-100');
-    }
+    };
 
     useEffect(() => {
-        function createCardBoard() {
-            const picsGenerated = pics?.concat(...pics)
-            const shuffledArray = shuffleArray(picsGenerated)
-            setpicsArray(shuffledArray)
-        }
-        createCardBoard()
-    }, [])
+        const createCardBoard = () => {
+            const picsGenerated = pics.concat(...pics);
+            const shuffledArray = shuffleArray(picsGenerated);
+            setPicsArray(shuffledArray);
+        };
+        createCardBoard();
+    }, []);
 
     useEffect(() => {
         let intervalId;
         if (isRunning) {
-            intervalId = setInterval(() => setTime(time + 1), 10);
+            intervalId = setInterval(() => setTime((time) => time + 1), 10);
         }
         return () => clearInterval(intervalId);
     }, [isRunning, time]);
 
-    useEffect(() => { 
-        if (creds === 20) {
-            setIsRunning(false);
-        }
-    }, [creds])
-
     useEffect(() => {
         if (creds === 20) {
+            setIsRunning(false);
             setShowMsg(true);
             setDarkness('brightness-50');
+            setTimeout(() => setShowMsg(false), 10000);
         }
-        setTimeout(() => {
-            setShowMsg(false);
-        }, 10000);
-    }, [creds])
-    
+    }, [creds]);
 
     return (
         <div className="md:h-screen md:flex md:items-center md:justify-center">
-            {(time < 6000 && showMsg) ? (
-                <h2 className="absolute z-50 font-bold font-bubblegum bg-gradient-to-r from-yellow-300 via-yellow-300
-                to-yellow-300 inline-block text-transparent bg-clip-text drop-shadow-2xl font-outline-2 mt-5 lg:text-10xl 
-                md:text-8xl max-sm:text-5xl max-sm:mt-20 max-sm:ml-15 lg:ml-20">Great job!</h2>
-            ) : ("")}
-            {(time > 6000 && showMsg) ? (
-                <h2 className="absolute z-50 font-bold font-bubblegum bg-gradient-to-r from-yellow-300 via-yellow-300
-                to-yellow-300 inline-block text-transparent bg-clip-text drop-shadow-2xl font-outline-2 mt-5 lg:text-10xl 
-                md:text-8xl max-sm:text-5xl max-sm:mt-20 max-sm:ml-4 lg:ml-20">You can do better!</h2>
-            ) : ("")}
+            {showMsg && (
+                <h2 className={`absolute z-50 font-bold font-bubblegum bg-gradient-to-r from-yellow-300 via-yellow-300
+                    to-yellow-300 inline-block text-transparent bg-clip-text drop-shadow-2xl font-outline-2 mt-5 lg:text-10xl 
+                    md:text-8xl max-sm:text-5xl max-sm:mt-20 max-sm:ml-15 lg:ml-20`}
+                >
+                    {time < 6000 ? "Great job!" : "You can do better!"}
+                </h2>
+            )}
             <div className="h-full justify-center fixed">{creds === 20 && <ConfettiExplosion config={confetti} />}</div>
             <div className="columns-1 lg:ml-12 lg:mr-5 md:ml-6 md:mr-1 max-sm:columns-3 max-sm:justify-center max-sm:my-5 max-sm:flex 
                 max-sm:items-center max-sm:ml-0 max-sm:mr-0">
@@ -131,7 +112,8 @@ function Game() {
                     Creds: {creds}
                 </h3>
                 <button className="font-sofia bg-gradient-to-r from-pink-300 via-yellow-200 to-pink-200 border-yellow-200 py-3 px-5
-                border-2 rounded-full text-base text-black font-bold max-sm:py-1 max-sm:px-3" onClick={startOver}>Start over
+                border-2 rounded-full text-base text-black font-bold max-sm:py-1 max-sm:px-3" onClick={startOver}>
+                    Start over
                 </button>
                 <div className="stopwatch-container lg:mt-5 md:mt-5 max-sm:ml-1 max-sm:w-15">
                     <p className="text-white text-center">
@@ -143,21 +125,21 @@ function Game() {
             </div>
             <div className={`grid lg:grid-cols-7 gap-4 lg:m-12 md:grid-cols-4 md:m-6 max-sm:grid-cols-4 max-sm:gap-3 max-sm:m-5 
                 max-sm:mt-3 ${darkness}`}>
-            <h2 className="flex items-center justify-center font-mooli lg:text-2xl md:text-sm max-sm:text-xs gradient-text">
-                Unicorn<br></br>Memory
-            </h2>
-            {picsArray?.map((pic, index) => {
-                return (
-                    <div key={index} onClick={() => flipPic(pic, index)} >
-                        <img src={isCardPicked(pic, index) ? pic : darkBackCard} 
-                            alt="mem_card" className={`rounded-lg`} 
+                <h2 className="flex items-center justify-center font-mooli lg:text-2xl md:text-sm max-sm:text-xs gradient-text">
+                    Unicorn<br></br>Memory
+                </h2>
+                {picsArray.map((pic, index) => (
+                    <div key={index} onClick={() => flipPic(pic, index)}>
+                        <img
+                            src={isCardPicked(pic, index) ? pic : darkBackCard}
+                            alt="mem_card"
+                            className="rounded-lg"
                         />
                     </div>
-                )
-            })}
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Game;
